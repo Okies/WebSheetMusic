0bf2b0bee8de6de82738897f8f6e7366b88d3966
Okies <ja00800@nate.com>
2021-09-26 18:27:49 +0900

    main page redirect

--- src/main/java/com/cscs/cscore/MainController.java
+++ src/main/java/com/cscs/cscore/MainController.java
@@ -1,13 +1,22 @@
 package com.cscs.cscore;
 
+import com.cscs.cscore.service.ScoreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RequiredArgsConstructor
+@Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String heartbeat() {
-        return "server is alive";
+    private final ScoreService scoreService;
+
+    @GetMapping({"", "/"})
+    public String getScores(Model model) {
+
+        model.addAttribute("scores", scoreService.findAll());
+
+        return "scores/list";
     }
 }
--- src/main/java/com/cscs/cscore/controller/ScoreController.java
+++ src/main/java/com/cscs/cscore/controller/ScoreController.java
@@ -9,7 +9,6 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Log4j2
 @RequiredArgsConstructor
